44556ecfcef7956aaba50102b903ff9ff24e048b
Maryann Niederman <maryann.e.niederman@gmail.com>
2021-03-05 21:52:31 -0500

    updating changes

--- assets/java.js
+++ assets/java.js
@@ -1,29 +1,32 @@
-var searchBtnEl = document.querySelector('searchBtn');
-var cityInputEl = document.querySelector('searchWeather');
+var searchBtnEl = document.querySelector(".search-Btn");
+var cityInputEl = document.querySelector(".searchWeather");
+var tempEl = document.querySelector(".temperature");
+var humEl = document.querySelector(".humidity")
+var wSEl = document.querySelector(".windSpeed")
+var uvIndexEl = document.querySelector(".uvI")
+var cityDateEl = document.querySelector(".cityDate")
 
-
-
-
-var searchbtnAction = function(event){
+var thisSucks = function (event) {
     event.preventDefault();
-    var city = cityInputEl;
-
-    if (city){
-        cityName(city);
-    }
+    var cityInput = cityInputEl.value.trim();
+    cityName(cityInput);
+    
 }
 
 
-      
 var cityName = function (cityName) {
-    var apiUrl = ('https;//www.api.openweathermap.org/data/2.5/weather?q=' + 'city name')
-
-    fetch(apiUrl)
+    fetch('https://www.api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=75c3ab4ae18755f10a323f2a0172dbf2')
     .then(function (response) {
         return response.json();
-    }) .then(function(data) {
-        console.log(data);
+    }) .then(function(data, cityName) {
     });
+};
+
+var citySearch = JSON.parse(localStorage.getItem('searchHistory'))
+function saveToList(cityInput){
+    citySearch.push(cityInput);
+    localStorage.setItem('searchHistory', JSON.stringify(citySearch));
+    renderSearchHistory();
 }
 
-searchBtnEl.addEventListener('click', searchbtnAction)
+searchBtnEl.addEventListener('click', thisSucks);
