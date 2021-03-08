7150db974410bcda9470280539b062b0da90f9ff
Maryann Niederman <maryann.e.niederman@gmail.com>
2021-03-08 00:18:49 -0500

    updating ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Weather-API
 
+## Table of Contents
+
+[*Description](#Description)
+
+[* Requirements](#Requirements)
+
+[* Installation](#Installation)
+
+[* Contributing](#Contributing)
+
+[* Credits](#Credits)
+
+
+## Description
+
+This repository houses a Weather Webpage. This weather webpage utilizes the Open Weather API to allow a user to view current and future forecast information for a searched city.
+
+### Deployed Website
+
+![deployed](./assets/images/image1.png)
+
+## Requirements
+```
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -11,3 +34,23 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+```
+
+## Installation
+
+No need for any installation for this project.
+
+See deployed site here: https://meniederman.github.io/ProfessionalPortfolio/
+
+See repository here: https://github.com/meniederman/ProfessionalPortfolio
+
+
+## Contributing
+
+Any feedback is encouraged and appreciated for future improvements.
+
+## Credits
+
+Maryann Niederman 2021
+Open Weather Free API
+
--- assets/java.js
+++ assets/java.js
@@ -1,19 +1,19 @@
 var searchBtnEl = document.querySelector(".search-Btn");
 var cityInputEl = document.querySelector(".searchWeather");
-var tempEl = document.querySelector(".temperature");
-var humEl = document.querySelector(".humidity")
-var wSEl = document.querySelector(".windSpeed")
-var uvIndexEl = document.querySelector(".uvI")
-var cityDateEl = document.querySelector(".cityDate")
+var cityDateEl = document.querySelector(".cityDate");
+var futureDateEl = document.querySelector(".future");
 
 var thisSucks = function (event) {
     event.preventDefault();
     var cityInput = cityInputEl.value.trim();
+    var futureInput = futureDateEl.value.trim();
     if(cityInput) {
-        cityInputEl.textContent = cityInput;
+        cityInputEl.textContent(cityInput);
     cityS(cityInput);
       }
-    
+      if(futureInput){
+          futureDateEl.textContent(futureInput);
+          forecastS(futureInput);
 }
 
 
@@ -29,6 +29,26 @@ var cityUrl  ='https://api.openweathermap.org/data/2.5/weather?q=' +cityName+'&a
         return cityName.appendChild.cityDateEl;
     }
 };
+
+var forecastS = function (cityName) {
+    var cityUrl  = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=' +cityName+ '&cnt=5&appid=6051ccf89f20e16cb1ece2484634b69c'
+    fetch(cityUrl)
+    .then(function(response){
+        return response.json();
+        console.log(response)
+    }) .then(function(data) {
+    });
+    then.function(response);{
+        var forecastS = (".card-body") 
+        forecastS.empty();
+        for (var i = 0; i < 5; i++){
+            var ugh = <div class = "card-group bg-info col-12"></div>;
+            forecastS.append(ugh)
+        }
+    }
+};
+
+}
 
 var citySearch = JSON.parse(localStorage.getItem('searchHistory'))
 function saveToList(cityInput){
